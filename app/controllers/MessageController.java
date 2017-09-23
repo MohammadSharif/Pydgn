b67752034602a614f142586aa166fe0279e494ea
Andrew Deaver <deaver.andrew@gmail.com>
2017-09-23 02:27:56 -0400

    Mostly merged

---
+++ app/controllers/MessageController.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import javax.inject.*;
+import play.mvc.*;
+import play.data.Form;
+import play.data.FormFactory;
+import models.Message;
+
+public class MessageController extends Controller {
+	@Inject FormFactory formFactory;
+
+	public Result parseMessage() {
+		// Get the POST data
+		Form<Message> form = formFactory.form(Message.class).bindFromRequest();
+
+		if (form.data().size() == 0) {
+			return badRequest("No data");
+		}
+
+		MessageParser mP = new MessageParser();
+		String message = form.get().message;
+		String userName = mP.extractUserName(message);
+		String extractedMessage = mP.extractMessage(message);
+		if (extractedMessage == null) {
+			return badRequest("No message");
+		}
+		return ok(extractedMessage);
+	}
+
+	private static class MessageParser {
+
+		/**
+		 * All messages must begin with @username so to extract the username we
+		 * get the substring from 1 - the first occurence of a space.
+		 * @param String message [message being sent over the Pydgn service]
+		 * @return the username the message is directed to
+		 */
+		public String extractUserName(String message){
+		  String userName = message.substring(1, message.indexOf(" "));
+		  return userName;
+		}
+
+		/**
+		 * Extracts the actual message text from the data sent through the chatbot.
+		 * @param  String message       [message being sent over Pydgn service]
+		 * @return the true message meant to be sent to the recipient
+		 */
+		public String extractMessage(String message){
+		  int startMessageIndex = message.substring(0, 1).equals("@") ? message.indexOf(" ") : 0;
+		  int endMessageIndex = message.contains("/end") ? message.indexOf("/end") : message.length();
+		  String extractedMessage = message.substring(startMessageIndex, endMessageIndex);
+		  return extractedMessage;
+		}
+
+		public boolean messageEnded(String message){
+		  return message.contains("/end");
+		}
+	}
+}
